dcda099ce84f18acce14a0a8e24cca0c071314ed
digi0ps <sriru1998@gmail.com>
2018-02-23 22:03:53 +0530

    Fetch search results of Memex for query.

--- src/search-injection/constants.js
+++ src/search-injection/constants.js
@@ -1,4 +1,8 @@
 // Constants
+
+// Limit for the number of search results to be fetched
+export const LIMIT = 5
+
 // regex - Regular Expression for the search url
 // container - ID of the container to append elements
 
