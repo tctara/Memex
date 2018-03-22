8bef53dd1d994b9b7382dc0a2580f7bf9a1f858e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-03-22 13:30:06 +0700

    Write tests for import item iteration and marking off
    - iteration used as main progress thing -> keep requesting new chunks to go through
    - marking off happens after each item is processed; they are removed from their chunk

--- src/imports/background/import-state.test.js
+++ src/imports/background/import-state.test.js
@@ -51,11 +51,10 @@ describe('Import items derivation', () => {
         await state._initFromCache()
 
         // State counts should now equal to the mock Cache's new data
-        expect(state.counts).toEqual(
-            expect.objectContaining({ ...DATA.fakeCacheCounts }),
-        )
+        expect(state.counts).toEqual(DATA.fakeCacheCounts)
     })
 
+    // Will set up import items state and ensure counts are valid
     const testEstimateCounts = async () => {
         // Try to fetch ests (should invoke the ItemCreator -> DataSources to estimate counts, if cache expired)
         const counts = await state.fetchEsts()
@@ -67,8 +66,9 @@ describe('Import items derivation', () => {
             b: DATA.bmUrls.length,
         })
 
-        // Check that those same counts are cached on the State instance's mock Cache
-        expect(state._cache.counts).toEqual({ ...counts })
+        // Check that those same counts are cached on the State instance's mock Cache, and also the State instance
+        expect(state._cache.counts).toEqual(counts)
+        expect(state.counts).toEqual(counts)
     }
 
     test('counts can be calculated (cache miss)', async () => {
@@ -87,5 +87,54 @@ describe('Import items derivation', () => {
 
         // Run same estimate counts test again with filled cache
         await testEstimateCounts()
+    })
+
+    test('import items can be iterated through', async () => {
+        await testEstimateCounts()
+
+        const bookmarkItemUrls = []
+        const historyItemUrls = []
+
+        // For each item in each chunk, save the URL as bookmark/history and
+        for await (const { chunk } of state.fetchItems()) {
+            Object.values(chunk).forEach(item => {
+                if (item.type === 'h') {
+                    historyItemUrls.push(item.url)
+                } else {
+                    bookmarkItemUrls.push(item.url)
+                }
+            })
+        }
+
+        expect(bookmarkItemUrls).toEqual(DATA.bmUrls)
+        expect(historyItemUrls).toEqual(DATA.histUrls)
+    })
+
+    test('import items can be removed/marked-off', async () => {
+        await testEstimateCounts()
+
+        const itemsChunkIterable = state.fetchItems()
+
+        // Remove the first item of first chunk (should be bookmarks)
+        const { value: valA } = await itemsChunkIterable.next()
+        await state.removeItem(valA.chunkKey, Object.keys(valA.chunk)[0])
+
+        // Counts should have changed (-1 bookmark)
+        expect(state.counts.completed).toEqual({ h: 0, b: 1 })
+        expect(state.counts.remaining).toEqual({
+            h: DATA.histUrls.length,
+            b: DATA.bmUrls.length - 1,
+        })
+
+        // Remove the first item of first chunk (should be history)
+        const { value: valB } = await itemsChunkIterable.next()
+        await state.removeItem(valB.chunkKey, Object.keys(valB.chunk)[0])
+
+        // Counts should have changed (-1 hist)
+        expect(state.counts.completed).toEqual({ h: 1, b: 1 })
+        expect(state.counts.remaining).toEqual({
+            h: DATA.histUrls.length - 1,
+            b: DATA.bmUrls.length - 1,
+        })
     })
 })
